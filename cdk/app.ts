d1717a2c44aa404e7947b2d358a656206a630807
hokyunrhee <hokyun.rhee@gmail.com>
2023-11-06 08:21:12 +0900

    feat: pulumi 추가

--- cdk/app.ts
+++ cdk/app.ts
@@ -1,6 +1,8 @@
 import * as cdk from "aws-cdk-lib"
 
+import { PulumiStack } from "./stacks/pulumi.stack"
 import { SlackBotStack } from "./stacks/slack-bot.stack"
 
 const app = new cdk.App()
-new SlackBotStack(app, `SlackBotStack`)
+new SlackBotStack(app, "SlackBotStack")
+new PulumiStack(app, "PulumiStack")
